bd24519e0a1a6559abf0521f6a29ce93647b7c2d
umesh patil <patilumesh3689@gmail.com>
2025-06-02 17:21:09 +0530

    Add project files.

---
+++ BankDetailShowAssign/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BankDetailShowAssign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accnotxt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showbtn
+            // 
+            this.showbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showbtn.Location = new System.Drawing.Point(449, 107);
+            this.showbtn.Name = "showbtn";
+            this.showbtn.Size = new System.Drawing.Size(83, 23);
+            this.showbtn.TabIndex = 0;
+            this.showbtn.Text = "Show";
+            this.showbtn.UseVisualStyleBackColor = true;
+            this.showbtn.Click += new System.EventHandler(this.showbtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(227, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(305, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Account No :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // accnotxt
+            // 
+            this.accnotxt.Location = new System.Drawing.Point(227, 107);
+            this.accnotxt.Multiline = true;
+            this.accnotxt.Name = "accnotxt";
+            this.accnotxt.Size = new System.Drawing.Size(187, 23);
+            this.accnotxt.TabIndex = 3;
+            this.accnotxt.TextChanged += new System.EventHandler(this.accnotxt_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.accnotxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.showbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox accnotxt;
+    }
+}
+
---
+++ BankDetailShowAssign/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Data.SqlClient;
+using SqlCommand = Microsoft.Data.SqlClient.SqlCommand;
+using SqlDataAdapter = Microsoft.Data.SqlClient.SqlDataAdapter;
+
+namespace BankDetailShowAssign
+{
+    public partial class Form1: Form
+    {
+        public Microsoft.Data.SqlClient.SqlConnection con { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accnotxt_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void showbtn_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-RJ7FDJE\\SQLEXPRESS;Initial Catalog=mydb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+            // Define connection properly
+            using (Microsoft.Data.SqlClient.SqlConnection con = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT * FROM accdetails WHERE accNo = @accNo";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        // Use TryParse to prevent runtime errors
+                        if (int.TryParse(accnotxt.Text, out int accNo))
+                        {
+                            cmd.Parameters.AddWithValue("@accNo", accNo);
+
+                            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                            DataSet ds = new DataSet();
+                            sda.Fill(ds, "Accdetails");
+
+                            dataGridView1.DataSource = ds.Tables["Accdetails"].DefaultView;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid Account Number. Please enter a numeric value.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            } 
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            accnotxt.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
+            accnotxt.ReadOnly = true;
+
+        }
+    }
+}
